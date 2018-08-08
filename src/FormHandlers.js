f6543cf61298d3ebf70ec9b7961251b292e3c035
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2018-08-08 15:26:09 +0200

    added event handlers for onkeyup editing the string objects

--- src/FormHandlers.js
+++ src/FormHandlers.js
@@ -1,31 +1,31 @@
 export function update_cff_version(payload) {
-    console.log('About to update cff_version with value ' + payload.value)
+    this.ccf_version = payload.value;
 }
 
 export function update_date_released(payload) {
-    console.log('About to update date_released with value ' + payload.value)
+    this.date_released = payload.value;
 }
 
 export function update_doi(payload) {
-    console.log('About to update doi with value ' + payload.value)
+    this.doi = payload.value;
 }
 
 export function update_message(payload) {
-    console.log('About to update message with value ' + payload.value)
+    this.message = payload.value;
 }
 
 export function update_license(payload) {
-    console.log('About to update license with value ' + payload.value)
+    this.license = payload.value;
 }
 
 export function update_repository_code(payload) {
-    console.log('About to update repository_code with value ' + payload.value)
+    this.repository_code = payload.value;
 }
 
 export function update_title(payload) {
-    console.log('About to update title with value ' + payload.value)
+    this.title = payload.value;
 }
 
 export function update_version(payload) {
-    console.log('About to update version with value ' + payload.value)
+    this.version = payload.value;
 }
